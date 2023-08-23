77921d3623ed10f325ffc86eef3091071b878486
xiaocai <1643013940@qq.com>
2023-08-23 11:28:55 +0800

    完善功能

--- container.go
+++ container.go
@@ -14,19 +14,23 @@ type Container interface {
 	Boot()
 	Instance(key string, instance any)
 	Bind(key string, f CallbackFunc)
-	singleton(key string, f CallbackFunc)
+	Singleton(key string, f CallbackFunc)
 	Make(key string) any
 }
 
 func New() Container {
 	return &container{
-		instances: &sync.Map{},
+		instances:  make(map[string]any),
+		l:          &sync.Mutex{},
+		singletons: make(map[string]CallbackFunc),
 	}
 }
 
 type container struct {
-	instances *sync.Map
-	providers []ServiceProvider
+	instances  map[string]any
+	providers  []ServiceProvider
+	l          *sync.Mutex
+	singletons map[string]CallbackFunc
 }
 
 func (c *container) Boot() {
@@ -44,27 +48,61 @@ func (c *container) AddServiceProvider(sp ServiceProvider) {
 }
 
 func (c *container) Instance(key string, instance any) {
-	c.instances.Store(key, instance)
+	c.l.Lock()
+	defer c.l.Unlock()
+	if c.hasInstance(key) {
+		return
+	}
+	c.instances[key] = instance
 }
 
-func (c *container) singleton(key string, f CallbackFunc) {
-	//TODO implement me
-	panic("implement me")
+func (c *container) Singleton(key string, f CallbackFunc) {
+	c.l.Lock()
+	defer c.l.Unlock()
+	if c.hasSingleton(key) {
+		return
+	}
+	c.singletons[key] = f
 }
 
 func (c *container) Bind(key string, f CallbackFunc) {
-	c.instances.Store(key, f)
+	c.Instance(key, f)
 }
 
 func (c *container) Make(key string) any {
-	if v, ok := c.instances.Load(key); ok {
-		switch tmp := v.(type) {
+	c.l.Lock()
+	defer c.l.Unlock()
+	if v, ok := c.instances[key]; ok {
+		switch t := v.(type) {
 		case CallbackFunc:
-			return tmp()
+			return t()
 		default:
 			return v
 		}
 	}
 
+	if c.isSingleton(key) {
+		c.instances[key] = c.singletons[key]()
+		return c.instances[key]
+	}
+
 	return nil
+}
+
+func (c *container) hasInstance(key string) bool {
+	if _, ok := c.instances[key]; ok {
+		return true
+	}
+	return false
+}
+
+func (c *container) hasSingleton(key string) bool {
+	if _, ok := c.singletons[key]; ok {
+		return true
+	}
+	return false
+}
+
+func (c *container) isSingleton(key string) bool {
+	return c.hasSingleton(key)
 }
--- container_test.go
+++ container_test.go
@@ -2,7 +2,6 @@ package gioc
 
 import (
 	"fmt"
-	"sync"
 	"testing"
 )
 
@@ -17,16 +16,8 @@ func NewAppServiceProvider(c Container) ServiceProvider {
 }
 
 func (a *AppServiceProvider) Register() {
-	var s *Student
-	once := &sync.Once{}
 	a.c.Bind("student", func() any {
-		once.Do(func() {
-			s = &Student{
-				ID:   "1001",
-				Name: "Aim",
-			}
-		})
-		return s
+		return NewStudent()
 	})
 }
 
@@ -37,6 +28,13 @@ func (a *AppServiceProvider) Boot() {
 type Student struct {
 	ID   string
 	Name string
+}
+
+func NewStudent() *Student {
+	return &Student{
+		ID:   "1001",
+		Name: "Aim",
+	}
 }
 
 func TestNew(t *testing.T) {
@@ -53,4 +51,24 @@ func TestNew(t *testing.T) {
 	s2 := c.Make("student").(*Student)
 	fmt.Println(s2.ID)
 	fmt.Println(s2.Name)
+}
+
+func BenchmarkNew(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = &Student{
+			ID:   "1001",
+			Name: "Aim",
+		}
+	}
+}
+
+func BenchmarkIOC(b *testing.B) {
+	c := New()
+	c.AddServiceProvider(NewAppServiceProvider(c))
+	c.Boot()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = c.Make("student").(*Student)
+	}
 }
